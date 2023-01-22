92b3fee016d83f2e5fa2a7aecca2bde693664767
jgsheppa <james.sheppard@avisio.ai>
2023-01-22 12:17:28 +0100

    create go project and add fileserver

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jgsheppa/jgsheppa.github.io
+
+go 1.19
+
+require github.com/go-chi/chi v1.5.4
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	// Index handler
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi"))
+	})
+
+	// Create a route along /files that will serve contents from
+	// the ./data/ folder.
+	workDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	filesDir := http.Dir(filepath.Join(workDir, "images"))
+	FileServer(r, "/images", filesDir)
+
+	http.ListenAndServe(":3000", r)
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
+}
